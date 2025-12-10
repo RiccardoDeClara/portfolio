902bc1ed1d5252dede9e7e0ea800a74eabc00071
Riccardo De Clara <riccardo.declara@gmail.com>
2025-12-10 23:48:46 +0100

    light-dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,16 @@
 
 /* import sections */
+import Header from "../components/sections/Header";
 import HeroBanner from "../components/sections/HeroBanner";
 import AboutMe from "../components/sections/AboutMe";
 import Experience from "../components/sections/Experience";
 import Skills from "../components/sections/Skills";
 import GetInTouch from "../components/sections/GetInTouch";
-import wave from "../../public/wave.svg";
+
 export default function Home() {
   return (
     <div className="container">
-      <header className="main-header">
-        <div className="logo"><h3>Riccardo De Clara</h3></div>
-        <nav>
-          <ul className="nav-links col-2">
-            <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#contact" className="btn-primary small">Contact</a></li>
-            <li className="d-flex">
-              <i className="bi bi-translate px-1 d-flex align-items-center">
-              </i>
-              <select id="select-lang" className="form-select form-select-sm">
-                <option value="en">EN</option>
-                <option value="it">IT</option>
-              </select>
-            </li>
-            {/* <li> */}
-            {/* <div className="form-check form-switch">
-              <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDefault" />
-            </div> */}
-            {/* </li> */}
-          </ul>
-        </nav>
-      </header>
+      <Header />
 
       <main>
         <HeroBanner />
---
+++ src/components/sections/Header.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useEffect, useState } from 'react';
+
+export default function Header() {
+  const [theme, setTheme] = useState(false);
+
+  const handleThemeChange = () => {
+    setTheme((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (theme) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }, [theme]);
+
+  useEffect(() => (
+    console.log(theme)
+  ), [theme])
+
+  return (
+    <header className="main-header">
+      <div className="logo"><h3>Riccardo De Clara</h3></div>
+      <nav>
+        <ul className="nav-links col-2">
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#contact" className="btn-primary small">Contact</a></li>
+          <li className="d-flex">
+            <i className="bi bi-translate px-1 d-flex align-items-center">
+            </i>
+            <select id="select-lang" className="form-select form-select-sm">
+              <option value="en">EN</option>
+              <option value="it">IT</option>
+            </select>
+          </li>
+          <li onClick={handleThemeChange} className=''>
+            <i className={`bi bi-sun-fill button-theme ${theme ? 'd-none' : ''}`} style={{ fontSize: '15px', padding: '5px' }}></i>
+            <i className={`bi bi-moon-fill button-theme ${!theme ? 'd-none' : ''}`} style={{ fontSize: '15px', padding: '5px' }}></i>
+          </li>
+        </ul>
+      </nav>
+    </header >
+  );
+}
